cc44e2d8b2242f23bf57ecd44ff1c73bd72b6e35
SeanIvers <seanivers55@gmail.com>
2021-12-08 11:12:38 -0500

    Add add to head function

--- doubly_linked_lists.py
+++ doubly_linked_lists.py
@@ -25,3 +25,13 @@ class DoublyLinkedList:
     def __init__(self):
         self.head_node = None
         self.tail_node = None
+
+    def add_to_head(self, new_value):
+        new_head = Node(new_value)
+        current_head = self.head_node
+        if current_head:
+            current_head.set_prev_node(new_head)
+            new_head.set_next_node(current_head)
+        self.head_node = new_node
+        if self.tail_node == None:
+            self.tail_node = self.head_node
